1086230968d10e2ce3c9569549c87c744e0e1d0f
JunaidRathore <66040197+JunaidRathore@users.noreply.github.com>
2020-08-17 02:14:10 +0500

    Add files via upload

---
+++ precache-manifest.76a8fab36faceaf390f54497dda122c2.js
@@ -0,0 +1,414 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a331015e35343bda25b15bec7f7d9d9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36a093280e6caac8582d",
+    "url": "/static/js/2.c21f0fd5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c21f0fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad4f3db3fde53e22b81",
+    "url": "/static/js/main.5e013001.chunk.js"
+  },
+  {
+    "revision": "a4552920b9e9934b3685",
+    "url": "/static/js/runtime-main.6b39261f.js"
+  },
+  {
+    "revision": "50337a2a3d6a5e6d0d1dde7e8d713957",
+    "url": "/static/media/Burberry-Blush.50337a2a.png"
+  },
+  {
+    "revision": "67420f6422dc2fe5f771dc77efe4bd88",
+    "url": "/static/media/Burberry-Body.67420f64.png"
+  },
+  {
+    "revision": "0db56992450e3f7cc55204b173a4896c",
+    "url": "/static/media/Burberry-Brit-Sheer.0db56992.png"
+  },
+  {
+    "revision": "3aa0db1a3a96a17386c088b0c98dba89",
+    "url": "/static/media/Burberry-Her.3aa0db1a.png"
+  },
+  {
+    "revision": "6bbcda50fa255d45d95ca19cf24aa03b",
+    "url": "/static/media/Burberry-Intense.6bbcda50.png"
+  },
+  {
+    "revision": "76ea7bc532f562cfd1d048d7e5998be9",
+    "url": "/static/media/Burberry-My.76ea7bc5.png"
+  },
+  {
+    "revision": "6e6ed34f75f433bf235fda14cdfaccff",
+    "url": "/static/media/Burberry-SoFar.6e6ed34f.png"
+  },
+  {
+    "revision": "950cd39031c0e3bc678ab53d1b07497d",
+    "url": "/static/media/Burberry-The-Beat.950cd390.png"
+  },
+  {
+    "revision": "1969da9708f747800cd72cfa5db3c30e",
+    "url": "/static/media/CK-All.1969da97.png"
+  },
+  {
+    "revision": "89b09ce574146b242bdbd2c265813080",
+    "url": "/static/media/CK-Euphoria.89b09ce5.png"
+  },
+  {
+    "revision": "9920d5b59aaed67b2f1078c6b846521f",
+    "url": "/static/media/CK-Everyone.9920d5b5.png"
+  },
+  {
+    "revision": "8b0db913abf645df246a32c5da72689a",
+    "url": "/static/media/CK-Free.8b0db913.png"
+  },
+  {
+    "revision": "c091bead65a04fee46b02d217c5c269e",
+    "url": "/static/media/CK-Man.c091bead.png"
+  },
+  {
+    "revision": "ea5dabedf0f46983e6d307ce1cd0b2ee",
+    "url": "/static/media/CK-Obsession.ea5dabed.png"
+  },
+  {
+    "revision": "c727d73bb412ebbac524b63b2de6ddd8",
+    "url": "/static/media/CK-Reveal.c727d73b.png"
+  },
+  {
+    "revision": "7b0c96b179780eb15d3d095942450603",
+    "url": "/static/media/Chanel-Allure-Sensuelle.7b0c96b1.png"
+  },
+  {
+    "revision": "884bc2ff8b5c52f93d579bc5be940fff",
+    "url": "/static/media/Chanel-Coco-Noir.884bc2ff.png"
+  },
+  {
+    "revision": "50de83ff197b6420babb3edce8ee7bd5",
+    "url": "/static/media/Chanel-Fraiche.50de83ff.png"
+  },
+  {
+    "revision": "9ee9bee615fb52f4f351896001d73163",
+    "url": "/static/media/Chanel-Gabrielle.9ee9bee6.png"
+  },
+  {
+    "revision": "b52affd38c127f0d9864452edd10df8c",
+    "url": "/static/media/Chanel-Mademoiselle.b52affd3.png"
+  },
+  {
+    "revision": "c8ca7ffa0165a40cefbd5467d7d34ea5",
+    "url": "/static/media/Chanel-No-19.c8ca7ffa.png"
+  },
+  {
+    "revision": "c4049a2d427e8d9b025fb6d33c10a008",
+    "url": "/static/media/Chanel-No-5-Leua.c4049a2d.png"
+  },
+  {
+    "revision": "8336237f7a9ed0e10c487d3141e421b6",
+    "url": "/static/media/Chanel-No-5.8336237f.png"
+  },
+  {
+    "revision": "80f6a6b2897146d3d8f3318da5f78972",
+    "url": "/static/media/Chanel-Tendre.80f6a6b2.png"
+  },
+  {
+    "revision": "e4ad1ad7299a29480ad1982796667ce4",
+    "url": "/static/media/EW-Anima.e4ad1ad7.png"
+  },
+  {
+    "revision": "52a94bc1b7f0737170289ca83b78b6b0",
+    "url": "/static/media/EW-Crave.52a94bc1.png"
+  },
+  {
+    "revision": "fcdaa41df106c1cfea49af3473c22205",
+    "url": "/static/media/EW-Delight.fcdaa41d.png"
+  },
+  {
+    "revision": "342f177becd9212a8855480887da7815",
+    "url": "/static/media/EW-Diva.342f177b.png"
+  },
+  {
+    "revision": "0723c85da888a3fa1953c948117b9abb",
+    "url": "/static/media/EW-Innoces.0723c85d.png"
+  },
+  {
+    "revision": "9efce26c3ca1b629fde820a13879e54d",
+    "url": "/static/media/EW-Roma.9efce26c.png"
+  },
+  {
+    "revision": "4d9158c531753409fd7153f228c644b3",
+    "url": "/static/media/EW-Trance.4d9158c5.png"
+  },
+  {
+    "revision": "0fa66a05a353ae20d851c4f4b42c7053",
+    "url": "/static/media/EW-Tropics.0fa66a05.png"
+  },
+  {
+    "revision": "41f77986681fca3057d1332dd7e8d68c",
+    "url": "/static/media/Eden-Alpha.41f77986.png"
+  },
+  {
+    "revision": "4fc3a05c60a464f7a80b45bddec7d500",
+    "url": "/static/media/Eden-Casnova.4fc3a05c.png"
+  },
+  {
+    "revision": "5409dcb0dab4241587e4f84c80a6dcac",
+    "url": "/static/media/Eden-Fleet.5409dcb0.png"
+  },
+  {
+    "revision": "286d74fd90b66384aca097c789d25909",
+    "url": "/static/media/Eden-Hombre.286d74fd.png"
+  },
+  {
+    "revision": "1a4537e60ba87dd59e5a38cb33ed6c56",
+    "url": "/static/media/Eden-Imperium.1a4537e6.png"
+  },
+  {
+    "revision": "fd6a1fe0a6a0d432353c6259fe6ea7ab",
+    "url": "/static/media/Eden-Monarc.fd6a1fe0.png"
+  },
+  {
+    "revision": "20b1ec6efba472264e82d5cda15ea5e3",
+    "url": "/static/media/Eden-Prime.20b1ec6e.png"
+  },
+  {
+    "revision": "3796b3e3fc663c2b118600c2797b53e9",
+    "url": "/static/media/Eden-timeus.3796b3e3.png"
+  },
+  {
+    "revision": "2f1bd17f535fd4971dec5c8148969c58",
+    "url": "/static/media/Eternity-Plus.2f1bd17f.png"
+  },
+  {
+    "revision": "ea548c9719644a3b358b6f7c81e38b56",
+    "url": "/static/media/Eternity.ea548c97.png"
+  },
+  {
+    "revision": "fce0b408ed4ce8ab21c26632c477f7ef",
+    "url": "/static/media/Hugo-Boss-Dark-Blue.fce0b408.png"
+  },
+  {
+    "revision": "28a1a5b2e217b3cdbc6eb823ade16eaf",
+    "url": "/static/media/Hugo-Boss-Extreme.28a1a5b2.png"
+  },
+  {
+    "revision": "c32b3b2e8bac03ff02eb6301d5fa6c08",
+    "url": "/static/media/Hugo-Boss-Selection.c32b3b2e.png"
+  },
+  {
+    "revision": "1161fc41bcee20473cdd20b3cc5ee911",
+    "url": "/static/media/Hugo-Boss-Soul.1161fc41.png"
+  },
+  {
+    "revision": "7a6e378ad6af0e5a01462fc384d0c8f3",
+    "url": "/static/media/Hugo-Boss-The-Scent.7a6e378a.png"
+  },
+  {
+    "revision": "beaa22b2720540542ab22f17be7b16e8",
+    "url": "/static/media/J-Era.beaa22b2.png"
+  },
+  {
+    "revision": "6020961d9415f54f561a5cb4ff070c86",
+    "url": "/static/media/J-Generous.6020961d.png"
+  },
+  {
+    "revision": "d250e5f817642c80e52eb0b3297d4da7",
+    "url": "/static/media/J-Legacy.d250e5f8.png"
+  },
+  {
+    "revision": "3919a9e609d85c17752f99afdf625449",
+    "url": "/static/media/J-Men-Intensive.3919a9e6.png"
+  },
+  {
+    "revision": "d613be1f2694b7d0efe21dd52678e2b3",
+    "url": "/static/media/J-Mika.d613be1f.png"
+  },
+  {
+    "revision": "487778467ef7b6b71baead2f72b46ca5",
+    "url": "/static/media/J-Rythm.48777846.png"
+  },
+  {
+    "revision": "5ea98405b0ea221369b814507fa36c44",
+    "url": "/static/media/J-Tareekh.5ea98405.png"
+  },
+  {
+    "revision": "082c376b4fce06c785d0f441981a38bb",
+    "url": "/static/media/JW-Azbah.082c376b.png"
+  },
+  {
+    "revision": "c1f2b5507e4dd4856478af6b84b772d8",
+    "url": "/static/media/JW-Bloom.c1f2b550.png"
+  },
+  {
+    "revision": "755d72cfd45467a645e25dfc4b92664d",
+    "url": "/static/media/JW-Essence.755d72cf.png"
+  },
+  {
+    "revision": "e9d8c3c3316c28a51e4aa2a9e5828ef6",
+    "url": "/static/media/JW-Flora.e9d8c3c3.png"
+  },
+  {
+    "revision": "0b06bb40deb85fc514f0ad6973289b2b",
+    "url": "/static/media/JW-Komal.0b06bb40.png"
+  },
+  {
+    "revision": "c75e055156eb35fb5e0b0071facea68d",
+    "url": "/static/media/JW-MoonLight.c75e0551.png"
+  },
+  {
+    "revision": "e82f961df28006e7006e3e980000acd4",
+    "url": "/static/media/JW-Pleasure.e82f961d.png"
+  },
+  {
+    "revision": "9312a1f0cde3d6810248fa6286c18fbe",
+    "url": "/static/media/JW-Pour-Femme.9312a1f0.png"
+  },
+  {
+    "revision": "8dbfa21a0fc0b0f3de42277269a44e44",
+    "url": "/static/media/Janan-Pour-Femme.8dbfa21a.png"
+  },
+  {
+    "revision": "b86e00d8c64eb0b3141943056fa8afee",
+    "url": "/static/media/Krone-Athletic.b86e00d8.png"
+  },
+  {
+    "revision": "442aaf1549b8bc70686f8d48b3fab99f",
+    "url": "/static/media/Krone-Brown-Champ.442aaf15.png"
+  },
+  {
+    "revision": "f4346de4f90b8426bdb91175ca2b5255",
+    "url": "/static/media/Krone-Classical.f4346de4.png"
+  },
+  {
+    "revision": "1c9453acb1d49e082ec9b1e72dbd79ed",
+    "url": "/static/media/Krone-Noir-AquaBlue.1c9453ac.png"
+  },
+  {
+    "revision": "dc68f2ee4b9f4c020c60ac6d6f0358fd",
+    "url": "/static/media/Krone-Noir-GasFree.dc68f2ee.png"
+  },
+  {
+    "revision": "e7b49f8ccbc7bc8a57396230fff62d6a",
+    "url": "/static/media/Krone-Noir-White.e7b49f8c.png"
+  },
+  {
+    "revision": "8d20cbd4adc14048f956a2d912b18f8f",
+    "url": "/static/media/Krone-Persona.8d20cbd4.png"
+  },
+  {
+    "revision": "c11f210ead4e85e99d329a756e797f57",
+    "url": "/static/media/Krone-Tempted-ManX.c11f210e.png"
+  },
+  {
+    "revision": "47b04875c9a4b71b3b3cd9662b3888c9",
+    "url": "/static/media/Lancome-Hypnose.47b04875.png"
+  },
+  {
+    "revision": "46c030c43d3d410c3c50101f620f3ff9",
+    "url": "/static/media/Lancome-Jasmin-Marzipane.46c030c4.png"
+  },
+  {
+    "revision": "80a986ed2d75da0783d4ed16aebdb548",
+    "url": "/static/media/Lancome-Miracle-Secret.80a986ed.png"
+  },
+  {
+    "revision": "d3b52a9c54354aeb415031de19b4e3d4",
+    "url": "/static/media/Lancome-Nuit-Tresor-Nude.d3b52a9c.png"
+  },
+  {
+    "revision": "a67244a3f1dc00f5acc593a42ab83c9b",
+    "url": "/static/media/Lancome-Nuit-Tresor.a67244a3.png"
+  },
+  {
+    "revision": "114c1e86822270f0a833d73407ee173d",
+    "url": "/static/media/Lancome-Rose.114c1e86.png"
+  },
+  {
+    "revision": "4008d3bc08fe042f8f3845fa6485d44a",
+    "url": "/static/media/Lancome-Sagamore.4008d3bc.png"
+  },
+  {
+    "revision": "2779b6ff0a919aa87119624e4862df60",
+    "url": "/static/media/Lancome-Tresor-InLove.2779b6ff.png"
+  },
+  {
+    "revision": "6cd227ecbeeb6f788b4497ca39432640",
+    "url": "/static/media/Lancome-Tresor-Mdnight-Rose.6cd227ec.png"
+  },
+  {
+    "revision": "ac9b38e7c5e5f65b6e8ad283a08c6442",
+    "url": "/static/media/Lomani-Chichissime.ac9b38e7.png"
+  },
+  {
+    "revision": "50dd5e613d45b5acc73b69c57126a790",
+    "url": "/static/media/Lomani-Code.50dd5e61.png"
+  },
+  {
+    "revision": "898e44cabb1da770a510dc528106e6ca",
+    "url": "/static/media/Lomani-Couture.898e44ca.png"
+  },
+  {
+    "revision": "fae92c784b0df4024c38639f4a1aa90b",
+    "url": "/static/media/Lomani-Intense-Code.fae92c78.png"
+  },
+  {
+    "revision": "f9d842febfdd6f88aa5ec97dc3437f5b",
+    "url": "/static/media/Lomani-Lomax.f9d842fe.png"
+  },
+  {
+    "revision": "f3be6beaf8f9e577b9b9fd46cc05d7d2",
+    "url": "/static/media/Lomani-Millionaire.f3be6bea.png"
+  },
+  {
+    "revision": "20d993be281321d4e085d27b894f7fc4",
+    "url": "/static/media/Lomani-New-Town.20d993be.png"
+  },
+  {
+    "revision": "0b73a73278ac68e1b5cfe8d7a92013a9",
+    "url": "/static/media/Lomani-Pour-Homme.0b73a732.png"
+  },
+  {
+    "revision": "67155166410766eb258db2a55c28f25d",
+    "url": "/static/media/Lomani-Wild.67155166.png"
+  },
+  {
+    "revision": "7680437377388ca0dfba85bcac478f9a",
+    "url": "/static/media/Oud-Al-Junaid.76804373.png"
+  },
+  {
+    "revision": "0ea79fd56acd4cc4778978daea388200",
+    "url": "/static/media/Wasim-502.0ea79fd5.png"
+  },
+  {
+    "revision": "164a8d8ce50d7d913d63e6c3fa2f0736",
+    "url": "/static/media/about-img.164a8d8c.png"
+  },
+  {
+    "revision": "da59c594021a10355cc74f95238a0be4",
+    "url": "/static/media/c1.da59c594.jpg"
+  },
+  {
+    "revision": "95b382c68d3c5bac07fbf58fa73fb7c6",
+    "url": "/static/media/c2.95b382c6.jpg"
+  },
+  {
+    "revision": "e2f55027193b3949bb83a3c610bb28af",
+    "url": "/static/media/c3.e2f55027.jpg"
+  },
+  {
+    "revision": "1005667e0a091cade9c931719226b48e",
+    "url": "/static/media/krone-Noir-Black.1005667e.png"
+  },
+  {
+    "revision": "0b4431abea7628c1e5e5644a98bd16b6",
+    "url": "/static/media/mission-img.0b4431ab.jpg"
+  },
+  {
+    "revision": "2d72589d2b7555e57940a394cbf82d8c",
+    "url": "/static/media/vission-img.2d72589d.jpg"
+  }
+]);
